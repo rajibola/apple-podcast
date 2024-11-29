69f305226b5f2dbf52352b049789ad752ff1cef7
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2024-11-30 00:17:05 +0100

    chore: test if changes are pushed to repo

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,13 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {RootStackParamList} from '../../../App';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export const HomeScreen = ({navigation}: Props) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
       <TouchableOpacity onPress={() => navigation.navigate('Favourite')}>
         <Text>Go to Favs</Text>
@@ -15,3 +15,7 @@ export const HomeScreen = ({navigation}: Props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+});
